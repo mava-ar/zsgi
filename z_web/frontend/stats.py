3d3615b1b1b55533fd0d638373740428df607ca2
matuu <matu.varela@gmail.com>
2015-12-28 17:34:14 -0300

    Corrección de relaciones con registros luego de migracion

--- z_web/frontend/stats.py
+++ z_web/frontend/stats.py
@@ -9,7 +9,7 @@ from registro.models import Partediario
 
 
 def get_calculo_costo(costos, valores, horas_dia, total=0):
-    val = costos.get(valores["equipo__equipo__familia_equipo_id"], 0) * horas_dia * valores["dias_mes"]
+    val = costos.get(valores["registro_equipo__equipo__familia_equipo_id"], 0) * horas_dia * valores["dias_mes"]
     total += val
     return total, val
 
@@ -38,16 +38,16 @@ def calcular_item_costo(report, datos, no_prorrat, prorrat=[], multiplicador=1):
 def get_utlizacion_equipo(periodo):
     qs = Partediario.objects.filter(
         fecha__lte=periodo.fecha_fin, fecha__gte=periodo.fecha_inicio, situacion__id=1).exclude(
-        equipo__equipo_id=1).exclude(equipo_id__isnull=True).values(
-        'equipo__equipo_id', 'equipo__equipo__n_interno', 'funcion__funcion', 'operario__nombre',
-        'obra__obra', 'obra_id', 'equipo__equipo__familia_equipo_id').annotate(dias_mes=Count('id')).order_by(
-        '-obra_id', '-equipo__equipo__n_interno')
+        registro_equipo__equipo_id=1).exclude(registro_equipo__isnull=True).values(
+        'registro_equipo__equipo_id', 'registro_equipo__equipo__n_interno', 'funcion__funcion', 'operario__nombre',
+        'obra__obra', 'obra_id', 'registro_equipo__equipo__familia_equipo_id').annotate(dias_mes=Count('id')).order_by(
+        '-obra_id', '-registro_equipo__equipo__n_interno')
 
     processed = defaultdict(list)
     for result in qs:
         processed[result["obra__obra"]].append(result)
     result = dict(processed.items())
-    ids = list(set(x["equipo__equipo__familia_equipo_id"] for x in qs))
+    ids = list(set(x["registro_equipo__equipo__familia_equipo_id"] for x in qs))
     param = CostoParametro.objects.get(periodo=periodo)
     # calculo de lubricantes
     lubris = dict(LubricanteFluidosHidro.objects.filter(
@@ -103,7 +103,7 @@ def get_cc_on_periodo(periodo, totales):
             When(
                 partediario__fecha__gte=periodo.fecha_inicio,
                 partediario__fecha__lte=periodo.fecha_fin,
-                then='partediario__equipo__cant_combustible')
+                then='partediario__registro_equipo__cant_combustible')
         )
     )
     ).values_list('id', 'combustible'))
